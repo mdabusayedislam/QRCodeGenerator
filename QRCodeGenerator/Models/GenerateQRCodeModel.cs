b6a5f75c49ba808594500cba37e4e9264a390d1b
mdabusayedislam <abusayediit@gmail.com>
2022-07-14 18:16:59 +0600

    Add project files.

---
+++ QRCodeGenerator/Models/GenerateQRCodeModel.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace QRCodeGenerator.Models
+{
+    public class GenerateQRCodeModel
+    {
+        [Display(Name = "Enter QR Code Text")]
+        public string QRCodeText{get;set;}
+    }
+}
